85b1e1cb3634ef3d1c7602ddee0ac52e9d4214b7
Jerry Chang <jerry1215jerry@gmail.com>
2015-01-15 09:59:02 +0800

    Initial Commit

---
+++ GPSAssignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GPSAssignment
+//
+//  Created by JERRY CHANG on 1/15/15.
+//  Copyright (c) 2015 com.jerrysays. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
